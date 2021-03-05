4ca4b70dfe5b895edabd8e3ad9246ff6c1ef8989
DESKTOP-PRP1LLE\sanek <sanek33333@gmail.com>
2021-03-05 20:42:15 +0200

    Warm Up code finished need to add text

--- Lab_01/Functions.py
+++ Lab_01/Functions.py
@@ -24,7 +24,22 @@ def AlocateMatrix(index,matrix):
         return matrix
     elif index == 3:
         return np.uint8(matrix - np.amin(matrix))
-
+def PlotingDiscreteConvolution(x,h,y,titile):
+    axis_font = {'fontname':'Arial', 'size':'18'}
+    fig , subplotlist = plt.subplots(3,1,figsize=[10,5],sharey=True,sharex=True)
+    plt.tight_layout()
+    subplotlist[0].set_title(titile)
+    subplotlist[2].set_xlabel("n",**axis_font)
+    subplotlist[0].set_ylabel("x[n]",**axis_font)
+    subplotlist[1].set_ylabel("h[n]",**axis_font)
+    subplotlist[2].set_ylabel("y[n]",**axis_font)
+    for subplot in subplotlist:
+        subplot.set_ylim((0,np.amax(y)+2))
+    #ploting stuff
+    subplotlist[0].stem([i for i in range(1,np.size(x)+1)],x)
+    subplotlist[1].stem([i for i in range(1,np.size(h)+1)],h)
+    subplotlist[2].stem([i for i in range(1,np.size(y)+1)],y)
+    fig.show()
 
 if __name__ == "__main__ ":
     StripesUnit8((200,300),(200,50),6,[10 ,50 ,100, 150, 200 ,250])
